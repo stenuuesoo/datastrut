7e7025efa53c6709e7b922c826d5c1356971d490
Sten Uuesoo <sten.uuesoo@gmail.com>
2022-10-14 16:06:18 +0300

    Added not prime number list

--- prime.py
+++ prime.py
@@ -3,10 +3,13 @@ import time
 numberList = list(range(1, 100))
 newPrimeList = []
 controlPrimeList = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+notPrimes = []
 
 def checkPrime(numberList):
     if numberList:
         if numberList[0] > 1:
+            if primeFactor(numberList[0]):
+                notPrimes.append(numberList[0])
             if not primeFactor(numberList[0]): 
                 newPrimeList.append(numberList[0])
         checkPrime(numberList[1:])  
@@ -18,17 +21,20 @@ def primeFactor(primeNumberCandidate):
 
 def controlList(newPrimeList):
     return newPrimeList == controlPrimeList
-
+        
+def printNumbers():
+    print('\nPrime Numbers - {}'.format(newPrimeList))       
+    print('Not Prime Numbers - {}'.format(notPrimes))
+        
 def runCode():
     st = time.time()
     checkPrime(numberList)
+    printNumbers()
     et = time.time()
     return (et - st) * 1000
 
 def main():
     print('\nExecution time: {} seconds. Prime Numbers - {} '.format(runCode(),controlList(newPrimeList)))
-    for primes in newPrimeList:
-        print(primes, end=' ')
     
 if __name__ == "__main__":
     main()
