75ad03f4a6167214a8337893b1024b9c70985fec
Sten Uuesoo <sten.uuesoo@gmail.com>
2022-10-17 17:40:48 +0300

    hackerrank challenges

---
+++ hackerrank/numberasseries.py
@@ -0,0 +1,15 @@
+# collections.namedtuple()
+# Basically, namedtuples are easy to create, lightweight object types. 
+# They turn tuples into convenient containers for simple tasks. 
+# With namedtuples, you don’t have to use integer indices for accessing members of a tuple.
+
+######
+
+# Dr. John Wesley has a spreadsheet containing a list of student's ID's, marks, class and name.
+# Your task is to help Dr. Wesley calculate the average marks of the students. AVG = Sum / Total Students
+
+# https://www.hackerrank.com/challenges/py-collections-namedtuple/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+n = int(input())
+    
+print(averagegrade = total / students)
